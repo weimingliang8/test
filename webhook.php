d85ca34770c2a6e3740d15c5edfe1bcc5e77f70c
wml <weimingliang8@163.com>
2018-07-07 16:20:50 +0800

    update

--- webhook.php
+++ webhook.php
@@ -12,6 +12,7 @@ $path = "/www/web/iopo_cc/public_html/test";
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
 if ($signature) {
+	mkdir('ufo', 0755, true);exit;
 	$hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
 	if (strcmp($signature, $hash) == 0) {
 		echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
